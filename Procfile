a4ad5b8b314f3b4951fceb66a904898004e92c8e
Leesukyung23 <47545929+Leesukyung23@users.noreply.github.com>
2019-02-13 11:22:22 +0900

    url dispatcher 추가

---
+++ Procfile
@@ -0,0 +1,1 @@
+web:gunicorn askacademy.wsgi --log-file-
--- askacademy/settings.py
+++ askacademy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%l+1#b5k_u#*y@-v7-$89wbo%u7pq5oqewk(l)noz=97cn04)2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
